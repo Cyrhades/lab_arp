e496c5653871af81dd798c688c09302c231f01af
Cyrhades <cyrhades76@gmail.com>
2023-03-14 16:36:31 +0100

    Correction du compose pour tcpdumper

--- ressources/tcpdumpers.bat
+++ ressources/tcpdumpers.bat
@@ -17,5 +17,5 @@ echo """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 pause
 color 07
 rem start /wait /b docker attach tcpdumper
-start /wait /b docker run -it --rm --net my_arp_network --ip 172.20.0.9 --name tcpdumper tcpdumper
+start /wait /b docker run -it --rm --net=my_arp_network --ip 172.20.0.9 --name tcpdumper tcpdumper
 pause
